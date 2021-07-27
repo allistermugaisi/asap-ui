936a0a694fa23d6a9c90ed764d2d2af03b42550b
allistermugaisi <allistermugaisi@gmail.com>
2021-07-27 14:41:38 +0300

    updated landing ui responsive

--- js/app.js
+++ js/app.js
@@ -28,11 +28,11 @@ let htmlcssArrow = document.querySelector('.htmlcss-arrow');
 htmlcssArrow.onclick = function () {
 	navLinks.classList.toggle('show1');
 };
-let moreArrow = document.querySelector('.more-arrow');
-moreArrow.onclick = function () {
-	navLinks.classList.toggle('show2');
-};
-let jsArrow = document.querySelector('.js-arrow');
-jsArrow.onclick = function () {
-	navLinks.classList.toggle('show3');
-};
+// let moreArrow = document.querySelector('.more-arrow');
+// moreArrow.onclick = function () {
+// 	navLinks.classList.toggle('show2');
+// };
+// let jsArrow = document.querySelector('.js-arrow');
+// jsArrow.onclick = function () {
+// 	navLinks.classList.toggle('show3');
+// };
