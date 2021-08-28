28e95edaac714c94ad85b52216d8442e1fb91b70
allistermugaisi <allistermugaisi@gmail.com>
2021-08-28 19:30:40 +0300

    added support section on landing ui

--- js/app.js
+++ js/app.js
@@ -37,12 +37,13 @@ for (i = 0; i < moreArrow.length; i++) {
 	};
 }
 
-let jsArrow = document.querySelector('.js-arrow');
-jsArrow.onclick = function () {
-	navLinks.classList.toggle('show3');
-};
+// let jsArrow = document.querySelector('.js-arrow');
+// jsArrow.onclick = function () {
+// 	navLinks.classList.toggle('show3');
+// };
 
 let slideIndex = 0;
+// showSlides();
 
 function showSlides() {
 	let i;
@@ -67,8 +68,9 @@ function myFunction(x) {
 
 let x = window.matchMedia('(max-width: 576px)');
 myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
+x.addEventListener('load', myFunction); // Attach listener function on state changes
 
+// Product tabs
 function productTabs(event, tabName) {
 	let evt = event;
 	let i, x, tablinks;
@@ -85,130 +87,3 @@ function productTabs(event, tabName) {
 	document.getElementById(tabName).style.display = 'block';
 	evt.currentTarget.className += ' tab-bg';
 }
-
-// Search functionality
-function filterFunction(that, event) {
-	let container, input, filter, li, input_val;
-	container = document.querySelector(that).closest('.searchable');
-	input_val = container.querySelector('input').value.toUpperCase();
-
-	if (['ArrowDown', 'ArrowUp', 'Enter'].indexOf(event.key) != -1) {
-		keyControl(event, container);
-	} else {
-		li = container.querySelector('ul li');
-		li.each(function (i, obj) {
-			if (
-				document.querySelector(this).text().toUpperCase().indexOf(input_val) >
-				-1
-			) {
-				document.querySelector(this).show();
-			} else {
-				document.querySelector(this).hide();
-			}
-		});
-
-		container.querySelector('ul li').removeClass('selected');
-		setTimeout(function () {
-			container
-				.querySelector('ul li:visible')
-				.first()
-				.classList.add('selected');
-		}, 100);
-	}
-}
-
-function keyControl(e, container) {
-	if (e.key == 'ArrowDown') {
-		if (container.querySelector('ul li').classList.contains('selected')) {
-			if (
-				container
-					.querySelector('ul li:visible')
-					.index(container.querySelector('ul li.selected')) +
-					1 <
-				container.querySelector('ul li:visible').length
-			) {
-				container
-					.querySelector('ul li.selected')
-					.removeClass('selected')
-					.nextAll()
-					.not('[style*="display: none"]')
-					.first()
-					.classList.add('selected');
-			}
-		} else {
-			container.querySelector('ul li:first-child').classList.add('selected');
-		}
-	} else if (e.key == 'ArrowUp') {
-		if (
-			container
-				.querySelector('ul li:visible')
-				.index(container.querySelector('ul li.selected')) > 0
-		) {
-			container
-				.querySelector('ul li.selected')
-				.removeClass('selected')
-				.prevAll()
-				.not('[style*="display: none"]')
-				.first()
-				.classList.add('selected');
-		}
-	} else if (e.key == 'Enter') {
-		container
-			.querySelector('input')
-			.val(container.querySelector('ul li.selected').text())
-			.blur();
-		onSelect(container.querySelector('ul li.selected').text());
-	}
-
-	container.querySelector('ul li.selected')[0].scrollIntoView({
-		behavior: 'smooth',
-	});
-}
-
-function onSelect(val) {
-	alert(val);
-}
-
-document.querySelector('.searchable input').focus(function () {
-	document
-		.querySelector(this)
-		.closest('.searchable')
-		.querySelector('ul')
-		.show();
-	document
-		.querySelector(this)
-		.closest('.searchable')
-		.querySelector('ul li')
-		.show();
-});
-document.querySelector('.searchable input').blur(function () {
-	let that = this;
-	setTimeout(function () {
-		document
-			.querySelector(that)
-			.closest('.searchable')
-			.querySelector('ul')
-			.hide();
-	}, 300);
-});
-
-document
-	.querySelector(document)
-	.addEventListener('click', '.searchable ul li', function () {
-		document
-			.querySelector(this)
-			.closest('.searchable')
-			.querySelector('input')
-			.val(document.querySelector(this).text())
-			.blur();
-		onSelect(document.querySelector(this).text());
-	});
-
-document.querySelector('.searchable ul li').hover(function () {
-	document
-		.querySelector(this)
-		.closest('.searchable')
-		.querySelector('ul li.selected')
-		.removeClass('selected');
-	document.querySelector(this).classList.add('selected');
-});
